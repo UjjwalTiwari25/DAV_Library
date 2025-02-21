35817475da7d838304d5aa9a0016a96712e48d68
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2025-02-21 14:21:45 +0530

    book deleted from fav

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -25,7 +25,7 @@ router.post("/place-order", authenticateToken, async (req, res) => {
             success: Success,
             message: "Order placed successfully",
         });
-        
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({message: "Internal Server Error"});
