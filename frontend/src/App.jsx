43f691038619141831240a9bf2c501cd4a6e1d6d
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2025-02-22 23:49:35 +0530

    tailwindcss setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import 'react';
+import './App.css';
 import Home from './pages/Home';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
 
 const App = () => {
   return (
     <div>
       <Home/>
+      <Navbar/>
+      <Footer/>
       
     </div>
   )
